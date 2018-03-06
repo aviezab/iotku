81653d42bd94dcd6acbda57743bf418bb05728e5
Alvito <alvito.raidhy21@gmail.com>
2018-03-06 17:46:42 +0700

    testing dashboard

--- dashboard_testing/test.js
+++ dashboard_testing/test.js
@@ -8,7 +8,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0px";
+    document.getElementById("main").style.marginLeft = "50px";
     document.getElementById("scndSidenav").style.width = "50px";
 
 }
