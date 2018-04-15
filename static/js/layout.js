57f41cd595569fe6b62a4c160cfe166acbe97670
alvito <alvito.raidhy21@gmail.com>
2018-04-15 16:23:28 +0700

    test

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.btn-reload-devices').click(function(){
-		$.get( "ajax/test.html", function(data) {
+		$.get( "http://apione.iotku.id/get_device_list", function(data) {
 			$('#device-list').empty();
-			for(var k in data) {
+			var json = JSON.parse(data);
+			for(var k in json['result']) {
 				var li = '<li><a href="/device?device_ip=' + k[0] + '">' + k[1] + '</a></li>';
 				$('#device-list').append(li);
 			}
