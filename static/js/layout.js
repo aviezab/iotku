8c0b7a50619b45b216c202a11153669805c6dba7
alvito <alvito.raidhy21@gmail.com>
2018-04-15 14:32:12 +0700

    minor tes

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  
-}
+    $('.btn-reload-devices').click(function(){
+		$.get( "ajax/test.html", function(data) {
+			$('#device-list').empty();
+			for(var k in data) {
+				var li = '<li><a href="/device?device_ip=' + k[0] + '">' + k[1] + '</a></li>'
+				$('#device-list').append(li)
+			}
+		});
+    });
+});
