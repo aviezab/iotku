6cfab406e042bac456d3471e1303a2790751bcde
alvito <alvito.raidhy21@gmail.com>
2018-04-15 15:20:11 +0700

    test

--- web.py
+++ web.py
@@ -41,5 +41,5 @@ def device():
         return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
+    app.secret_key = "secret key"
     app.run(debug=True,host='0.0.0.0', port=5000)
