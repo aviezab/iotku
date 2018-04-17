340b844c53e936b107cb9e02634ed0c1ff056ad4
alvito <alvito.raidhy21@gmail.com>
2018-04-18 04:47:55 +0700

    test

--- iotku_database.py
+++ iotku_database.py
@@ -2,10 +2,6 @@ from pymongo import MongoClient
 import hashlib
 
 class Iotku:
-	_db = None
-	_user_list = None
-	_device_list = None
-	_sensor_list = None
 	def __init__(
 					self,
 					client = MongoClient(),
@@ -19,8 +15,6 @@ class Iotku:
 		self.user_list = self.db[user_list]
 		self.device_list = self.db[device_list]
 		self.sensor_list = self.db[sensor_list]
-		global _db,_user_list,_device_list,_sensor_list
-		_db,_user_list,_device_list,_sensor_list = self.db, self.user_list, self.device_list, self.sensor_list
 		
 	def get_user_list(self):
 		return_list = [x for x in self.user_list.find({})]
@@ -29,8 +23,11 @@ class Iotku:
 	def find_user(self, user_info):
 		assert type(user_info) == type(dict())
 		result = self.user_list.find_one(user_info)
-		_id = result["_id"]
-		return User(_id)
+		if result:
+			_id = result["_id"]
+			return User(_id)
+		else:
+			return None
 		
 	def add_user(self, email, password):
 		inserted = self.user_list.insert_one({
@@ -40,17 +37,21 @@ class Iotku:
                                 "device_list":[]
 								})
 		_id = inserted.inserted_id
-		return User(_id)
+		return User(_id, self.user_list, self.device_list, self.sensor_list)
 	
 class User(Iotku):
-	def __init__(self, _id):
+	def __init__(self, _id, user_list, device_list, sensor_list):
+		self.user_list = user_list
+		self.device_list = device_list
+		self.sensor_list = sensor_list
+		
 		self._id = _id
-		self.user_document = super()._user_list.find_one({"_id":self._id})
-		self.email = super()._user_document["email"]
-		self.password = super()._user_document["password"]
+		self.user_document = self.user_list.find_one({"_id":self._id})
+		self.email = self.user_document["email"]
+		self.password = self.user_document["password"]
 		
 	def reload(self):
-		self.__init__(self._id)
+		self.__init__(self._id, self.user_list, self.device_list, self.sensor_list)
 		return True
 		
 	def change_user_info(self, user_info):
@@ -68,8 +69,8 @@ class User(Iotku):
 		mongo_id = self.device_list.insert_one({"device_name":device_name, "device_ip":device_ip})
 		self.user_document["device_list"].append({"device_ip":device_ip,"mongo_id":mongo_id})
 		self.user_list.save(self.device_document)
-		_id = result["_id"]
-		return Device(_id)
+		_id = mongo_id.inserted_id
+		return Device(_id, device_list, sensor_list)
 		
 	def remove_device(self, device_ip):
 		device_info = next((item for item in self.user_document["device_list"] if item["device_ip"] == device_ip), False)
@@ -86,12 +87,14 @@ class User(Iotku):
 		if device_info:
 			result = self.device_list.find_one({"_id":device_info["mongo_id"]})
 			_id = result["_id"]
-			return Device(_id)
+			return Device(_id, device_list, sensor_list)
 		else:
 			return False
 			
 class Device(User):
-	def __init__(self, _id):
+	def __init__(self, _id, device_list, sensor_list):
+		self.device_list = device_list
+		self.sensor_list = sensor_list
 		self._id = _id
 		self.device_document = self.device_list.find_one({"_id":self._id})
 		self.device_name = self.device_document["device_name"]
@@ -117,6 +120,8 @@ class Device(User):
 		mongo_id = self.sensor_list.insert_one({"sensor_name":sensor_name})
 		self.device_document["sensor_list"].append({"sensor_ip":sensor_ip,"mongo_id":mongo_id})
 		self.device_list.save(self.device_document)
+		_id = mongo_id.inserted_id
+		return Sensor(_id, sensor_list)
 		
 	def remove_sensor(self, sensor_id):
 		sensor_info = next((item for item in self.device_document["device_list"] if item["sensor_id"] == sensor_id), False)
@@ -132,12 +137,14 @@ class Device(User):
 		sensor_info = next((item for item in self.device_document["device_list"] if item["sensor_id"] == sensor_id), False)
 		if sensor_info:
 			result = self.sensor_list.find_one({"_id":sensor_info["mongo_id"]})
-			return result
+			_id = result["_id"]
+			return Sensor(_id, sensor_list)
 		else:
 			return False
 	
 class Sensor(Device):
-	def __init__(self,_id,):
+	def __init__(self, _id, sensor_list):
+		self.sensor_list = sensor_list
 		self._id = _id
 		self.sensor_document = self.sensor_list.find_one({"_id":self._id})
 		self.sensor_name = self.sensor_document["sensor_name"]
@@ -146,7 +153,7 @@ class Sensor(Device):
 		self.total_data_entry = self.sensor_document["total_data_entry"]
 		
 	def reload(self):
-		self.__init__(self._id)
+		self.__init__(self._id,self.sensor_list)
 		return True
 		
 	def change_sensor_info(self, sensor_info):
