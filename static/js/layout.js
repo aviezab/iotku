28fb67dc703431f1d773b71ff24a469a2906ef1f
alvito <alvito.raidhy21@gmail.com>
2018-04-18 12:40:56 +0700

    test

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.btn-reload-devices').click(function(){
-		$.get( "/api/get_device_list", function(data) 
+		$.get( "/api/get_device_list", function(data) {
 			$('#device-list').empty();
 			for(var k in data['result']) {
 				var a = '<a href="/device?device_ip=' + data['result'][k]['device_ip'] + '">' + data['result'][k]['device_name'] + '</a>'
