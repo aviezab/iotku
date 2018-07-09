e6749ac804732792e02042c03933388aceb10440
icarus213 <32793558+icarus213@users.noreply.github.com>
2018-07-09 18:12:42 +0700

    Update core.py

--- api/core.py
+++ api/core.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session, jsonify, url_for
+from flask import Blueprint, request, session, jsonify, url_for, redirect
 from . import api, iotku
 
 #---------------------CORE---------------------------
